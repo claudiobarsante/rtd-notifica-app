bcba06190d98285eba13bb5e425b014b97cf8206
Claudio Barsante <clbmribas@gmail.com>
2021-04-14 23:18:18 -0300

    basics tests - signInFailure and final state reducer

--- src/store/auth/auth.test.ts
+++ src/store/auth/auth.test.ts
@@ -1,19 +1,29 @@
 import sagaHelper from 'redux-saga-testing';
 import { call, put } from 'redux-saga/effects';
 import { signIn } from './sagas';
+import * as matchers from 'redux-saga-test-plan/matchers';
 
-import { signInRequest, setLoadingIndicator } from './actions';
+import { signInRequest, setLoadingIndicator, signInFailure, signInSuccess } from './actions';
 import { UserActionTypes } from './actions';
 import { LoadingIndicator } from '../../types/commom';
 import { expectSaga } from 'redux-saga-test-plan';
+import { Credentials } from './types';
 
+import sigInService from './../../services/authService';
+
+import authReducer from './reducer';
+const mockAction = {
+	type: UserActionTypes.SIGNIN_REQUEST,
+	payload: { email: '123@gmail.com', password: '123' },
+};
+/*Testando com o redux-saga-testing */
 // describe('Testing signIn', () => {
 // 	const sigInService = jest.fn();
 // 	const mockAction = {
 // 		type: UserActionTypes.SIGNIN_REQUEST,
 // 		payload: { email: '123@gmail.com', password: '123' },
 // 	};
-// 	const it = sagaHelper(signIn());
+// 	const it = sagaHelper()
 
 // 	// it('should put setLoadingIndicator', result => {
 // 	// 	expect(result).toEqual(
@@ -26,13 +36,58 @@ import { expectSaga } from 'redux-saga-test-plan';
 // 	// });
 // });
 
-const mockAction = {
-	type: UserActionTypes.SIGNIN_REQUEST,
-	payload: { email: '123@gmail.com', password: '123' },
-};
-
+/*testando com redux-saga-test-plan */
 it('setLoadingIndicator', () => {
 	return expectSaga(signIn, mockAction)
 		.put(setLoadingIndicator({ isLoading: true, activityText: 'carregando request' }))
-		.run();
+		.silentRun();
+});
+
+// it('Api', () => {
+// 	return expectSaga(signIn, mockAction)
+// 		.provide([[matchers.call.fn(sigInService), { email: '123@gmail.com', password: '123' }]])
+// 		.dispatch(signInSuccess({ code: 400, message: 'Usuário ou senha inválidos !' }))
+// 		.silentRun();
+// });
+const error = {
+	code: 400,
+	message: 'Usuário ou senha inválidos !',
+};
+
+const state = {
+	currentUser: {
+		apelido: '',
+		institucionalId: 0,
+		oficioId: 0,
+		userId: '',
+		userName: '',
+	},
+	error: { code: 400, message: 'Usuário ou senha inválidos !' },
+	expirationDate: undefined,
+	isAuthenticated: false,
+	loadingIndicator: {
+		isLoading: false,
+		activityText: 'acabou o request',
+	},
+	token: '',
+};
+it('Api error', () => {
+	return (
+		expectSaga(signIn, mockAction)
+			//.provide([[call(sigInService, { email: '12@gmail.com', password: '123' }), error]])
+			.dispatch(signInFailure(error))
+
+			.silentRun()
+	);
+});
+
+it('Api', () => {
+	return (
+		expectSaga(signIn, mockAction)
+			.withReducer(authReducer)
+			//.provide([[call(sigInService, { email: '12@gmail.com', password: '123' }), error]])
+			//.dispatch(signInFailure(error))
+			.hasFinalState(state)
+			.silentRun()
+	);
 });
