58c9f890d4351347c609e8897a237b945cf4300e
Claudio Barsante <clbmribas@gmail.com>
2021-04-24 19:47:34 -0300

    testing styled-react-modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -35,45 +35,4 @@ body{
     }
 
 
-/*Modal*/
-.react-modal-overlay{
-    background:rgba(0,0,0,0.5);
-
-    position:fixed;
-    top:0;
-    bottom:0;
-    left:0;
-    right:0;
-
-    display:flex;
-    align-items:center;
-    justify-content:center;
-
-}
-
-.react-modal-close{
-    background:transparent;
-    border:0;
-
-    position:absolute;
-    right:1.5rem;
-    top:1.5rem;
-
-    transition:filter 0.2s;
-
-    &:hover{
-        filter:brightness(0.8)
-    }
-}
-.react-modal-content{
-    width:100%;
-    max-width: 576px;
-
-    background:var(--background-modal);
-    border-radius:0.24rem;
-
-    padding:3rem;
-    position:relative;   
-  
-}
 `;
